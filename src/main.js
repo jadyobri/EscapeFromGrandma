a27894db5481d1b94fb16dfc8a5d4054a2ff2044
“ichury” <ichury@ucsc.edu>
2023-11-27 19:16:46 -0800

    please work

---
+++ src/main.js
@@ -0,0 +1,25 @@
+'use strict'
+
+let config = {
+    type: Phaser.WEBGL, 
+    render: {
+        pixelArt: true, 
+    }, 
+    width: 800, 
+    height: 600, 
+    scene: [Menu, Tutorial, Start, Level1, GameOver], //add Menu scene 
+    physics: {
+        default: 'arcade',
+        arcade: {
+            //debug: true,
+            gravity: {
+                x: 0,
+                y: 0
+            }
+        }
+    },
+    
+}
+
+
+let game = new Phaser.Game(config); 
