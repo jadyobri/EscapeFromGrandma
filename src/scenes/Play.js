0b516a265c39b37bc28644b61cb04eb4d3cb0ae7
Jack O'Brien <112610774+jadyobri@users.noreply.github.com>
2023-12-01 16:37:48 -0800

    Add files via upload
    I have added the grandma (code name lola).

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,17 +4,24 @@ class Play extends Phaser.Scene {
     }
 
     create(){
+
+       // this.jumped = 0;
         this.mainscreen = this.add.tileSprite(0, 0, 800, 600, 'mainscreen').setOrigin(0, 0); 
+       
+        //Player
         this.player = this.physics.add.sprite(game.config.width/4, game.config.height-150, 'character').setScale(1.5);  //character starting position 
+
         this.player.setCollideWorldBounds(true); 
+        
         cursors = this.input.keyboard.createCursorKeys();
         let inviswall = this.physics.add.sprite(game.config.width/2,game.config.height-60,'invisible');
+        
         inviswall.setSize(game.config.width, inviswall.height);
         inviswall.setDisplaySize(game.config.width, inviswall.height);
         console.log(inviswall.width);
         inviswall.body.setImmovable(true);
         this.physics.add.collider(this.player, inviswall);
-        inviswall.setVisible(false);
+        //inviswall.setVisible(false);
         //inviswall.setCollideWorldBounds(true);
        // inviswall.setSize(game.config.width, )
         this.direction = true; //right 
@@ -34,8 +41,35 @@ class Play extends Phaser.Scene {
         //cannot jump while firing
         //
         //if
-    } 
+        //
+        //Grandma
+        this.lola = this.physics.add.sprite(game.config.width/2, game.config.height-200, "grandma").setScale(2.0);
+        this.lola.setCollideWorldBounds(true);
+        this.lola.setSize(this.lola.width/2, this.lola.height/2)
+        //this.lola.setCircle(this.lola.width/3);
+        //this.lola.body.setOffset(this.lola.width/3,this.lola.height/4);
+        //this.lola.body.setImmovable(true);
+        this.lola.body.setOffset(this.lola.width/3, this.lola.height/2.5)
         
+        //this.lola.setSize(this.lola.width,this.lola.height);
+        this.physics.add.collider(this.lola, inviswall);
+        // this.physics.add.collider(this.player, this.lola, (player, lola)=> { 
+        //     if(this.direction == true){
+        //     this.player.anims.play("struggling-right",true);
+        //     }
+        //      else{
+        //         this.player.anims.play("struggling-left",true);
+        //     }
+        //  })
+        this.lola.body.setGravityY(300); 
+        inviswall.setVisible(false);
+
+        
+
+        
+
+    } 
+
 
     update(){
       
@@ -60,9 +94,18 @@ class Play extends Phaser.Scene {
             this.player.anims.play('idle-left'); 
         }
     }
-    if (cursors.up.isDown)
+    if (Phaser.Input.Keyboard.JustDown(cursors.up) && this.player.body.touching.down)
     {
-        this.player.setVelocityY(-330);
+        this.player.setVelocityY(-200);
+        //this.jumped = 0;
+    }   
+    if(this.checkCollision(this.player, this.lola) == true){
+        if(this.direction == true){
+                this.player.anims.play("struggling-right");
+                }
+                 else{
+                    this.player.anims.play("struggling-left");
+                } 
     }
     
 
@@ -93,5 +136,18 @@ class Play extends Phaser.Scene {
         let playerMovement;
         playerVector.length() ? playerMovement = 'running' : playerMovement = 'idle';
         this.player.play(playerMovement + '-' + playerDirection, true); */ 
-    } 
+   // } 
+}
+checkCollision(player,lola){
+    // if(player.x < lola.x + lola.width && player.x + player.width > lola.width && lola.x && player.y < lola.y + lola.height && player.height + player.y > lola.y){
+    //     return true;
+    // }
+    if(lola.x < player.x + player.width && lola.x + lola.width > player.width && player.x && lola.y < player.y + player.height && lola.height + lola.y > player.y){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
 }
